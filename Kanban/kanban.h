08ec521bfa3df2fd1b1b81465b536fe631a30e8e
overmann <nikitosin2005@gmail.com>
2025-06-18 21:31:16 +0300

    finished the work with kanban table

--- Kanban/kanban.h
+++ Kanban/kanban.h
@@ -4,6 +4,8 @@
 #include <QMainWindow>
 #include <QVBoxLayout>
 
+class TaskCard;
+
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class Kanban;
@@ -15,15 +17,24 @@ class Kanban : public QMainWindow
     Q_OBJECT
 
 public:
-    Kanban(QWidget *parent = nullptr);
+    explicit Kanban(QWidget *parent = nullptr);
     ~Kanban();
 
 private slots:
     void on_GoBackToMenu_clicked();
     void on_pushButton_2_clicked();
+    void moveTaskToNextColumn(TaskCard *task, QVBoxLayout *fromLayout);
 
 private:
     Ui::Kanban *ui;
+
     QVBoxLayout *notStartedLayout;
+    QVBoxLayout *startedLayout;
+    QVBoxLayout *completedLayout;
+
+    QString getProjectRootPath();
+    void saveTasksToFile();
+    void loadTasksFromFile();
 };
+
 #endif // KANBAN_H
